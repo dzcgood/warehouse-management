c1680a1681daa0a661a38c2b875f7e58f22b1a61
DzcGood <920402784@qq.com>
2023-07-23 23:44:47 +0800

    vue2项目的创建以及加入element-ui支持

---
+++ wms-web/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <WebIndex></WebIndex>
+  </div>
+</template>
+
+<script>
+
+import WebIndex from "@/components/WebIndex";
+export default {
+  name: 'App',
+  components: {
+    WebIndex
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 100%;
+}
+</style>
